a326d8b4663c9a5f638d8b0576a85d3c6d13ebab
Aditya Srivastava <adityasrivastava.united@gmail.com>
2023-10-01 02:23:59 +0530

    form submission completed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import logo from './assets/investment-calculator-logo.png';
 import './App.css';
 import Calculator from './components/Calculator';
 import Report from './components/Report';
-
+import {useState} from "react";
 
 function App() {
+
+  const [data, setData] = useState([]);
+  function dataUpdater(list){
+    setData(list);
+    console.log(data);
+  };
   return (
     <div>
         <header className='header'>
           <img src={logo} />
           <h1>Investment Calculator</h1>
         </header>
-        <Calculator />
+        <Calculator onSubmit={dataUpdater}/>
         <Report />
     </div>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,124 @@
+import {useState} from "react";
 
-const Calculator = () => {
+
+const Calculator = (props) => {
+
+    const [cs,setCS] = useState(0);
+    const [ys,setYS] = useState(0);
+    const [roi,setROI] = useState(0.0);
+    const [time,setTime] = useState(0);
+
+    function currentSavingHandler(e){    
+        if(isNaN(e.target.value)){
+            window.alert("Enter only numeric value");
+            setCS(0);
+            document.getElementById("CS").value = "";
+        }
+        else
+            {
+                setCS(e.target.value);
+            }
+    };
+
+
+    function yearlySavingHandler(e){    
+        if(isNaN(e.target.value)){
+            window.alert("Enter only numeric value");
+            setYS(0);
+            document.getElementById("YS").value = "";
+        }
+        else
+            {
+                setYS(e.target.value);
+            }
+    };
+    
+
+    function ROIHandler(e){    
+        if(isNaN(e.target.value)){
+            window.alert("Enter only numeric value");
+            setROI(0);
+            document.getElementById("ROI").value = "";
+        }
+        else
+            {
+                setROI(e.target.value);
+            }
+    };
+    
+
+    function durationHandler(e){    
+        if(isNaN(e.target.value)){
+            window.alert("Enter only numeric value");
+            setTime(0);
+            document.getElementById("time").value = "";
+        }
+        else
+            {
+                setTime(e.target.value);
+            }
+    };
+    
+    function resetHandler(){
+        setCS(0);
+        setYS(0);
+        setROI(0);
+        setTime(0);
+    };
+
+    function submitHandler(e){
+        e.preventDefault();
+        let si = 0;
+        let total_interest=0;
+        let CS=parseInt(cs);
+        const list = [];
+
+        for(let i=1;i<=time;i++)
+        {
+            si = (CS*roi)/100;
+            CS = (CS+parseInt(ys)+si);
+            total_interest+=si;
+
+            let jsonObject = {
+                "year":i,
+                "Total Saving":CS,
+                "Interest":si,
+                "Total Interest":total_interest,
+                "Invested Capital":(CS-si)
+            };
+            list.push(jsonObject);    
+            
+        }
+        props.onSubmit(list);
+    };
     return (
-        <form className="form">
+        <form className="form" onSubmit={submitHandler}>
             <div className="input-group">
                 <p>
                     <label>CURRENT SAVING($)</label><br/>
-                    <input type="text" />
+                    <input type="text" onChange={currentSavingHandler} id="CS"/>
+                    <label>{cs}</label>
                 </p>
                 <p>
                     <label>YEARLY SAVING($)</label>
-                    <input type="text" />
+                    <input type="text" onChange={yearlySavingHandler} id="YS"/>
+                    <label>{ys}</label>
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>EXPECTED INTEREST(% PER YEAR)</label>
-                    <input type="text" />
+                    <input type="text" onChange={ROIHandler} id="ROI"/>
+                    <label>{roi}</label>
                 </p>
                 <p>
                     <label>INVESTMENT DURATION(YEARS)</label>
-                    <input type="text" />
+                    <input type="text" onChange={durationHandler} id="Time"/>
+                    <label>{time}</label>
                 </p>
             </div>
             <p className="actions">
-                <button type="reset" className="buttonAlt">Reset</button>
+                <button type="reset" className="buttonAlt" onClick={resetHandler}>Reset</button>
                 <button type="Submit" className="button">Calculate</button>
             </p>
         </form>
