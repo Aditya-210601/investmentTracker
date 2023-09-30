0c079fa09565be7f3bf136f21e19f1b8733632e0
Aditya Srivastava <adityasrivastava.united@gmail.com>
2023-10-01 00:13:19 +0530

    Frontend completed

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,7 +1,19 @@
 
 const Report = () => {
     return (
-        <div>Table</div>
+        <div>
+            <table className="result">
+                <thead>
+                    <tr>
+                        <td>Year</td>
+                        <td>Total Savings</td>
+                        <td>Interest (Year)</td>
+                        <td>Total Interest</td>
+                        <td>Invested Capital</td>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     );
 };
 
